cde2e4baaec1662400a2835158ae1fc5734ae922
Emilio Wong <emiliowong@MacBook-Air-de-Emilio.local>
2021-01-06 23:27:55 -0700

    Refactored routes

--- routes.go
+++ routes.go
@@ -5,10 +5,18 @@ import (
 )
 
 func initializeRoutes() {
-	apiRoutes := router.Group("/api/v1")
+	v1Routes := router.Group("/api/v1")
 	{
-		apiRoutes.GET("/all", handlers.GetAllCryptos)
-		apiRoutes.GET("/fiat", handlers.GetCryptoWithSpecificFiat)
-		apiRoutes.GET("/crypto", handlers.GetCrypto)
+		v1Routes.GET("/all", handlers.GetAllCryptos)
+		cryptoRoutes := router.Group("/crypto")
+		{
+			cryptoRoutes.GET("/", handlers.GetCrypto)
+			cryptoRoutes.GET("/amount", handlers.GetCalculatedCryptoValue)
+		}
+		fiatRoutes := router.Group("/fiat")
+		{
+			fiatRoutes.GET("/", handlers.GetCryptoWithSpecificFiat)
+			fiatRoutes.GET("/amount")
+		}
 	}
 }
