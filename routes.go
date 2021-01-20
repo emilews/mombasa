cb1a30b8ae38e6c61cd38467cad4e2576ae6399f
Emilio Wong <emiliowong@MacBook-Air-de-Emilio.local>
2021-01-19 22:04:20 -0700

    Fixed routes

--- routes.go
+++ routes.go
@@ -8,15 +8,15 @@ func initializeRoutes() {
 	v1Routes := router.Group("/api/v1")
 	{
 		v1Routes.GET("/all", handlers.GetAllCryptos)
-		cryptoRoutes := router.Group("/crypto")
-		{
-			cryptoRoutes.GET("/", handlers.GetCrypto)
-			cryptoRoutes.GET("/amount", handlers.GetCalculatedCryptoValue)
-		}
-		fiatRoutes := router.Group("/fiat")
-		{
-			fiatRoutes.GET("/", handlers.GetCryptoWithSpecificFiat)
-			fiatRoutes.GET("/amount")
-		}
+	}
+	cryptoRoutes := router.Group("/api/c¡v1/crypto")
+	{
+		cryptoRoutes.GET("/", handlers.GetCrypto)
+		cryptoRoutes.GET("/amount", handlers.GetCalculatedCryptoValue)
+	}
+	fiatRoutes := router.Group("/api/v1/fiat")
+	{
+		fiatRoutes.GET("/", handlers.GetCryptoWithSpecificFiat)
+		fiatRoutes.GET("/amount")
 	}
 }
